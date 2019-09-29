ed893b611db963fffe18e799378cbbfd7df1456c
Minju Kang <minju@mymusictaste.com>
2019-09-29 23:28:56 +0900

    <feat>:add new modules for searchbar component

--- webpack.config.js
+++ webpack.config.js
@@ -14,6 +14,13 @@ module.exports = {
         test: /\.(js|jsx)$/,
         exclude: "/node_modules",
         use: ['babel-loader']
+      },
+      {
+        test: /\.css$/,
+        use: [
+            { loader: "style-loader" },
+            { loader: "css-loader" },
+          ]
       }
     ]
   },
