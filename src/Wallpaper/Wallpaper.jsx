6629a1314b908c465b1f89d751a459b421b59fb9
Minju Kang <52149296+MMT-minju@users.noreply.github.com>
2019-10-28 15:37:39 +0900

    <feat>:implement wallpaper

--- src/Wallpaper/Wallpaper.jsx
+++ src/Wallpaper/Wallpaper.jsx
@@ -1,16 +1,23 @@
 import React from 'react';
-import wallpaper from '../../asset/wallpaper/wallpaper1.png';
 import SearchBar from '../SearchBar/SearchBar';
+
+import wallpaper0 from '../../asset/wallpaper/wallpaper0.png';
+import wallpaper1 from '../../asset/wallpaper/wallpaper1.png';
+import wallpaper2 from '../../asset/wallpaper/wallpaper2.png';
+import wallpaper3 from '../../asset/wallpaper/wallpaper3.png';
+import wallpaper4 from '../../asset/wallpaper/wallpaper4.png';
+
 const Wallpaper = () => {
     // make random wallpaper call
     // 
     const banner = ['언제든 떠날 수 있어요.', '훌쩍 떠나고 싶을 때가 지금인가요?', '여행하기 참 좋은 날이죠?']
-
+    const wallpapers = [wallpaper0, wallpaper1, wallpaper2, wallpaper3, wallpaper4]
+    
     return (
-        <div id='wallpaper' style={{'backgroundImage': 'url(' + wallpaper + ')'}}>
-            <div id='banner'> 
+        <div id='wallpaper' style={{'backgroundImage': 'url(' + wallpapers[Math.floor(Math.random()*wallpapers.length)] + ')'}}>
+            {/* <div id='banner'> 
                 {banner[Math.floor(Math.random()*banner.length)]}
-            </div>
+            </div> */}
             <SearchBar/>
         </div>
     )
