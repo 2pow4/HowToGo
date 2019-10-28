592cde5aba046a68e5d9e28a40ff5dfefe9c0337
Minju Kang <52149296+MMT-minju@users.noreply.github.com>
2019-10-28 15:37:56 +0900

    <feat>: remove banner

--- src/Wallpaper/Wallpaper.jsx
+++ src/Wallpaper/Wallpaper.jsx
@@ -8,16 +8,10 @@ import wallpaper3 from '../../asset/wallpaper/wallpaper3.png';
 import wallpaper4 from '../../asset/wallpaper/wallpaper4.png';
 
 const Wallpaper = () => {
-    // make random wallpaper call
-    // 
-    const banner = ['언제든 떠날 수 있어요.', '훌쩍 떠나고 싶을 때가 지금인가요?', '여행하기 참 좋은 날이죠?']
-    const wallpapers = [wallpaper0, wallpaper1, wallpaper2, wallpaper3, wallpaper4]
-    
+
+    const wallpapers = [wallpaper0, wallpaper1, wallpaper2, wallpaper3, wallpaper4]    
     return (
         <div id='wallpaper' style={{'backgroundImage': 'url(' + wallpapers[Math.floor(Math.random()*wallpapers.length)] + ')'}}>
-            {/* <div id='banner'> 
-                {banner[Math.floor(Math.random()*banner.length)]}
-            </div> */}
             <SearchBar/>
         </div>
     )
