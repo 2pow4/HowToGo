6ab0646c28d60c21ca1adafd0e0c7fba9d61d07e
minju <52149296+minzoovv@users.noreply.github.com>
2019-12-03 00:23:28 +0900

    <feat>: revise some bugs
    - sorting error
    - date calculation
    - additional option for react-select

--- src/Result/Result.jsx
+++ src/Result/Result.jsx
@@ -1,115 +1,118 @@
 import React from "react";
 import axios from "axios";
-import {withRouter} from "react-router-dom";
-import "./result.css"
+import moment from "moment";
+import { withRouter } from "react-router-dom";
+import "./result.css";
 
-import Type from "./Type.jsx"
+import Type from "./Type.jsx";
 import Item from "./Item.jsx";
-import SortedBy from "./Select.jsx"
+import SortedBy from "./Select.jsx";
 
 class Result extends React.Component {
+  constructor(props) {
+    super(props);
 
-    constructor(props){
-        super(props);
-        
-        const query = new URLSearchParams(props.location.search);
-        const type = query.get('type');
-        
-        this.state = {
-            type: type || "bus",
-            resultList : [],
-            selectedOption: "departure_time"
-        };
+    const query = new URLSearchParams(props.location.search);
+    const type = query.get("type");
+
+    this.state = {
+      type: type || "bus",
+      resultList: [],
+      selectedOption: "departure_time"
+    };
+  }
+
+  handleSelected = e => {
+    this.setState({ selectedOption: e["value"] });
+  };
+
+  componentDidMount() {
+    this._getList();
+  }
+
+  componentDidUpdate(prevProps) {
+    let prevQuery = new URLSearchParams(prevProps.location.search);
+    let prevType = prevQuery.get("type");
+
+    let query = new URLSearchParams(this.props.location.search);
+    let type = query.get("type");
+
+    if (prevType !== type) {
+      this.setState({
+        type
+      });
     }
+  }
 
-    componentDidMount(){
-        this._getList();
-    }
-    
-    componentDidUpdate(prevProps){
-        let prevQuery = new URLSearchParams(prevProps.location.search);
-        let prevType = prevQuery.get('type');
+  _getList() {
+    axios
+      .get("result_list.json")
+      .then(response => {
+        this.setState({
+          resultList: response.data.resultList
+        });
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }
 
-        let query = new URLSearchParams(this.props.location.search);
-        let type = query.get('type');
-
-        console.log(type)
-
-        if(prevType !== type){
-            this.setState({
-                type
-            })
-        };
-    }
-    
-    _getList(){
-        axios.get("result_list.json")
-            .then(response => {
-                this.setState({
-                    resultList : response.data.resultList
-                })
-            })
-            .catch(error => {
-                console.log(error)
-            })
-    }
-
-
-    render(){
-        return(
-            <div className="result-box">
-                <Type />
-                <hr/>
-                <div className="sort-container">
-                    <span className="sort-text"> Sorted By </span>
-                    <SortedBy />
-                </div>
-                <div className="item-container">
-                {
-                    this.state.resultList.length > 0 ? (
-                        this.state.selectedOption == 'cost' ? (
-                        <Item className="item" list={
-                            this.state.resultList
-                            .filter( item => (
-                                item.type === this.state.type
-                            ))
-                            .sort((a, b) => 
-                                a.cost - b.cost
-                            )} 
-                        />
-                        ) : (
-                            this.state.selectedOption == 'departure_time' ? (
-                                <Item className="item" list={
-                                    this.state.resultList
-                                    .filter( item => (
-                                    item.type === this.state.type
-                                    ))
-                                    .sort((a, b) =>
-                                        new Date(a.departure_time).getTime() - new Date(b.departure_time).getTime()
-                                     )}
-                                />
-                            ) : (<Item className="item" list={
-                                    this.state.resultList
-                                    .filter( item => (
-                                    item.type === this.state.type
-                                    ))
-                                    .sort((a, b) =>
-                                        (new Date(a.arrival_time).getTime() - new Date(a.departure_time).getTime()) - (new Date(b.arrival_time).getTime() - new Date(b.departure_time).getTime())
-                                     )} 
-                                />
-                            )
-                        )
-                    ) : (
-                        <span>
-                            LOADING
-                        </span>
-                    )
-                }
-                </div>
-            </div>
-        )
-    }
-
+  render() {
+    return (
+      <div className="result-box">
+        <Type />
+        <hr />
+        <div className="sort-container">
+          <span className="sort-text"> Sorted By </span>
+          <SortedBy handleSelected={this.handleSelected} />
+        </div>
+        <div className="item-container">
+          {this.state.resultList.length > 0 ? (
+            this.state.selectedOption == "cost" ? (
+              <Item
+                className="item"
+                list={this.state.resultList
+                  .filter(item => item.type === this.state.type)
+                  .sort((a, b) => a.cost - b.cost)}
+              />
+            ) : this.state.selectedOption == "departure_time" ? (
+              <Item
+                className="item"
+                list={this.state.resultList
+                  .filter(item => item.type === this.state.type)
+                  .sort((a, b) => {
+                    moment(a.departure_time, "HH:mm").diff(
+                      moment(b.departure_time, "HH:mm"),
+                      "minutes"
+                    );
+                  })}
+              />
+            ) : (
+              <Item
+                className="item"
+                list={this.state.resultList
+                  .filter(item => item.type === this.state.type)
+                  .sort((a, b) => {
+                    let intervalA = moment(a.arrival_time, "HH:mm").diff(
+                      moment(a.departure_time, "HH:mm"),
+                      "minutes"
+                    );
+                    let intervalB = moment(b.arrival_time, "HH:mm").diff(
+                      moment(b.departure_time, "HH:mm"),
+                      "minutes"
+                    );
+                    console.log(intervalA, intervalB);
+                    return intervalA - intervalB;
+                  })}
+              />
+            )
+          ) : (
+            <span>LOADING</span>
+          )}
+        </div>
+      </div>
+    );
+  }
 }
 
 export default withRouter(Result);
