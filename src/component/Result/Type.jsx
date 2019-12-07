951fdda9f20f459d7b0bc8366a82f23a308cfef9
minju <52149296+minzoovv@users.noreply.github.com>
2019-12-07 23:23:54 +0900

    <fix>: change type css

--- src/component/Result/Type.jsx
+++ src/component/Result/Type.jsx
@@ -6,39 +6,49 @@ import { IconContext } from "react-icons";
 import "./type.css";
 
 const Type = props => (
-  <ul className="type">
-    <li className={props.type === "bus" ? "tab_type_on" : "tab_type_off"}>
-      <IconContext.Provider
-        value={{ color: "#332563", size: "20px", className: "type-icon" }}
-      >
-        <FaBus />
-      </IconContext.Provider>
-      <Link className="link" to="/results?type=bus">
-        {" "}
-        Bus{" "}
-      </Link>
+  <ul className="type layout-horizontal-space">
+    <li
+      className={props.type === "bus" ? "tab tab_type_on" : "tab tab_type_off"}
+    >
+      <div className="type-content">
+        <IconContext.Provider value={{ color: "#332563", size: "20px" }}>
+          <FaBus />
+        </IconContext.Provider>
+        <Link className="link" to="/results?type=bus">
+          {" "}
+          Bus{" "}
+        </Link>
+      </div>
     </li>
-    <li className={props.type === "train" ? "tab_type_on" : "tab_type_off"}>
-      <IconContext.Provider
-        value={{ color: "#332563", size: "20px", className: "type-icon" }}
-      >
-        <FaTrain />
-      </IconContext.Provider>
-      <Link className="link" to="/results?type=train">
-        {" "}
-        Train{" "}
-      </Link>
+    <li
+      className={
+        props.type === "train" ? "tab tab_type_on" : "tab tab_type_off"
+      }
+    >
+      <div className="type-content">
+        <IconContext.Provider value={{ color: "#332563", size: "20px" }}>
+          <FaTrain />
+        </IconContext.Provider>
+        <Link className="link" to="/results?type=train">
+          {" "}
+          Train{" "}
+        </Link>
+      </div>
     </li>
-    <li className={props.type === "plane" ? "tab_type_on" : "tab_type_off"}>
-      <IconContext.Provider
-        value={{ color: "#332563", size: "20px", className: "type-icon" }}
-      >
-        <FaPlane />
-      </IconContext.Provider>
-      <Link className="link" to="/results?type=plane">
-        {" "}
-        Plane{" "}
-      </Link>
+    <li
+      className={
+        props.type === "plane" ? "tab tab_type_on" : "tab tab_type_off"
+      }
+    >
+      <div className="type-content">
+        <IconContext.Provider value={{ color: "#332563", size: "20px" }}>
+          <FaPlane />
+        </IconContext.Provider>
+        <Link className="link" to="/results?type=plane">
+          {" "}
+          Plane{" "}
+        </Link>
+      </div>
     </li>
   </ul>
 );
