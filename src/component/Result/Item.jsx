b1999d0acfced0402e3505e1fc9937cd742fb818
minju <52149296+minzoovv@users.noreply.github.com>
2019-12-07 23:24:55 +0900

    <feat>:add elapse time display

--- src/component/Result/Item.jsx
+++ src/component/Result/Item.jsx
@@ -1,32 +1,43 @@
 import React from "react";
+import moment from "moment";
+import { AiOutlineSmallDash } from "react-icons/ai";
+import { IconContext } from "react-icons";
 
 import "./item.css";
 
 const Item = props => {
   return (
-    <ul>
-      {props.list.map((item, index) => (
-        <li key={index}>
-          <div className="container__result">
-            <div className="ratio-3 container__dep-arr">
-              <div className="ratio-1 content__dep">
-                <div className="content__dep-time">
-                  <div className="content__time">{item.departure_time}</div>
-                  <div>
-                    <span />
-                  </div>
-                </div>
+    <ul className="layout-vertical-center">
+      {props.list.map((item, index) => {
+        const elapseTime = moment(item.arrival_time, "HH:mm").diff(
+          moment(item.departure_time, "HH:mm"),
+          "minutes"
+        );
+        console.log(elapseTime)
+        return (
+          <li key={index} className="container__result">
+            <div className="ratio-3 container__dep-arr layout-horizontal-center">
+              <div className="ratio-1">
+                <div className="content__time">{item.departure_time}</div>
                 <div>{item.departure_location}</div>
               </div>
-              <div className="ratio-1 content__arr">
+              <div className="ratio-2  layout-horizontal-center">
+                <IconContext.Provider value={{ color: "#bbbbbb", size: "30px"}}>
+                  <AiOutlineSmallDash></AiOutlineSmallDash>{"   "} 
+                  <div className='content__elapse-time'>{`   ${elapseTime}분   `}</div> 
+                  {"   "}<AiOutlineSmallDash></AiOutlineSmallDash>
+                </IconContext.Provider> 
+                <div>&emsp;&emsp;</div>
+              </div>
+              <div className="ratio-1">
                 <div className="content__time">{item.arrival_time}</div>
                 <div>{item.arrival_location}</div>
               </div>
             </div>
             <div className="ratio-1 content__cost">{item.cost}원</div>
-          </div>
-        </li>
-      ))}
+          </li>
+        );
+      })}
     </ul>
   );
 };
